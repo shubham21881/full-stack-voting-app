96110ab924ef8087951d10a23711e0d3860d1f6b
shubham mourya <krishanmourya.sh@gmail.com>
2025-09-24 22:23:25 -0700

    some changes for frontend deployment in vercel

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: "*",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
