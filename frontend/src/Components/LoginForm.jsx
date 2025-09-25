b5f295ef46750e53403680f417d158d0a6d60c6d
shubham mourya <krishanmourya.sh@gmail.com>
2025-09-24 23:31:12 -0700

    fix login.jsx

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Button } from "./ui/button"
 import { NavLink, useNavigate } from "react-router-dom"
 import {
   Card,
