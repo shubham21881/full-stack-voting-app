18f5943a0aaaa34bc6146456f120017b7639390a
shubham mourya <krishanmourya.sh@gmail.com>
2025-09-24 23:06:25 -0700

    fix login error

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -7,9 +7,9 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "./ui/card"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
 
 export default function LoginForm({
   className,
